d0a6fe40cd2f8cb1490195837b61d171e496b169
Casey <caseyggareau@gmail.com>
2024-02-16 16:49:42 -0500

    changed grain pattern

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,9 @@ export default function Projects() {
     <main className="transition-colors duration-[2000ms] w-full h-full bg-[#CCC6B0] dark:bg-[#05080E] flex flex-col gap-4 items-center justify-center text-black dark:text-white">
       <h1>This page is currently under construction, please </h1>
       <Link href={"https://github.com/CaseSens"}>
-        <button>visit my github</button>
+        <button className="transition-all border-2 px-2 py-1 rounded-lg hover:bg-white/[0.2] hover:rounded-sm font-rubik font-bold">
+          visit my github
+        </button>
       </Link>
     </main>
   );
