7cc06f3f671411248028d420e06249258bd4a6c3
Casey <caseyggareau@gmail.com>
2024-02-17 21:03:40 -0500

    fixed again

--- src/app/hooks/viewport-hooks.tsx
+++ src/app/hooks/viewport-hooks.tsx
@@ -149,7 +149,7 @@ export const useViewportHooks = () => {
         dFactor = fromTop ? -1 : 1,
         tl = gsap.timeline({
           defaults: { duration: 1.25, ease: "power1.inOut" },
-          onComplete: () => (animating = false),
+          onComplete: () => {animating = false},
         });
       if (currentIndex >= 0) {
         // The first time this function runs, current is -1
