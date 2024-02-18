5f55480c37de173915191474678b5fde92bc5da7
Casey <caseyggareau@gmail.com>
2024-02-17 19:54:38 -0500

    added intro screen to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,77 +1,126 @@
 "use client";
 import "./page.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useViewportHooks } from "../hooks/viewport-hooks";
+import { Observer } from "gsap/all";
+
+type ProjectGallerySlide = {
+  title: string;
+  description: string;
+  skills: string[];
+  image: string;
+};
+
+const projectGallerySlides: ProjectGallerySlide[] = [
+  {
+    title: "This very portfolio!",
+    description: `
+    That's right! this portfolio itself is a showcase project!
+    It's made completely by hand in Next.js, as a way to display
+    my passion for art, colors, and programming!
+    `,
+    skills: ["Next.js", "React", "Tailwind", "HTML5", "CSS"],
+    image: "/portfolio_self.jpg",
+  },
+  {
+    title: "EFC",
+    description: `
+    An FFMPEG-based media file converter made with Electron.js.
+    This project was designed as a user-friendly and resource-efficient alternative to Handbrake, a popular FFMPEG media converter.
+    It's completely open-source, and uses JS instead of React to be as lightweight as possible.
+    `,
+    skills: ["Electron.js", "HTML5", "CSS"],
+    image: "/efc.jpg",
+  },
+  {
+    title: "CBS",
+    description: `
+    A batch/bash script creation tool made to increase accessibility in making terminal-level programs.
+    Easily create terminal scripts, completely written in english.
+    Made in Java so it it's designed to run everywhere.
+    `,
+    skills: ["Java", "Swing"],
+    image: "/cbs.jpg",
+  },
+  {
+    title: "Fractal Creation Simulator",
+    description: `
+    A simplistic tool for creating square-shaped fractals using collisions and trails.
+
+    `,
+    skills: ["Javascript", "HTML5", "CSS", "HTMLCanvas"],
+    image: "/frac_create.jpg",
+  },
+];
 
 export default function Projects() {
   const { applyObserver } = useViewportHooks();
+  const TRANSITION_DELAY = 2;
+  const [introDismissed, setIntroDismissed] = useState<boolean>(false);
+  const [introScreenOpacity, setIntroScreenOpacity] = useState<0 | 1>(1);
+
   useEffect(() => {
-    const observer = applyObserver();
+    let observer: Observer;
+    if (introDismissed) {
+      observer = applyObserver();
+    }
 
     return () => {
-      observer.kill();
+      if (observer) {
+        observer.kill();
+      }
     };
-  }, []);
+  }, [introDismissed]);
+
+  const handleDismissed = () => {
+    
+  }
 
   return (
     <main className="relative transition-colors duration-[2000ms] w-dvw h-dvh bg-bluestone text-textcream m-0 p-0 font-poppins">
-      <section className="proj-section first">
-        <div className="outer">
-          <div className="inner">
-            <div
-              style={{
-                backgroundImage: "url('flat-mountains_org.svg')",
-              }}
-              className="bg transition-colors duration-[2000ms] one bg-[#CCC6B0] dark:bg-bluestone"
-            >
-              <h2 className="section-heading">PROJECT 1</h2>
-            </div>
+      {!introDismissed ? (
+        <div className="w-full h-full flex items-center justify-center fixed top-0">
+          <div className="flex flex-col justify-start items-center gap-4 text-center">
+            <h2 className="section-heading">WELCOME TO MY PROJECTS</h2>
+            <span>
+              <p>This is an interactive gallery</p>
+              <p> You can view all of my projects simply by scrolling</p>
+            </span>
+            <button className="p-4 border-2 rounded-lg" onClick={() => setIntroDismissed(true)}> Click me to get started </button>
           </div>
         </div>
-      </section>
-      <section className="proj-section second">
-        <div className="outer">
-          <div className="inner">
-            <div
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/portfolio_self.jpg')`,
-              }}
-              className="bg two"
-            >
-              <h2 className="section-heading">PROJECT 1</h2>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="proj-section third">
-        <div className="outer">
-          <div className="inner">
-            <div
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/cbs.jpg')`,
-              }}
-              className="bg three"
-            >
-              <h2 className="section-heading">PROJECT 2</h2>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="proj-section">
-        <div className="outer">
-          <div className="inner">
-            <div
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/efc.jpg')`,
-              }}
-              className="bg four"
-            >
-              <h2 className="section-heading">PROJECT 3</h2>
-            </div>
-          </div>
-        </div>
-      </section>
+      ) : (
+        <>
+          {projectGallerySlides.map((slide) => (
+            <ProjectSection slide={slide} useGrad={true} />
+          ))}
+        </>
+      )}
     </main>
   );
 }
+
+interface ProjectSectionProps {
+  slide: ProjectGallerySlide;
+  useGrad?: boolean;
+}
+
+const ProjectSection: React.FC<ProjectSectionProps> = ({ slide, useGrad }) => {
+  return (
+    <section className="proj-section second">
+      <div className="outer">
+        <div className="inner">
+          <div
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${slide.image}')`,
+            }}
+            className="bg two"
+          >
+            <h2 className="section-heading">{slide.title}</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
